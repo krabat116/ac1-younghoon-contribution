22863fcee96994c2e6f2852d03250433d83465ba
Beauy152 <42170077+Beauy152@users.noreply.github.com>
2024-08-23 14:27:59 +1000

    feat: :bug: added seeding via migration

---
+++ backend/migrations/0002_seed.sql
@@ -0,0 +1,9 @@
+-- Migration number: 0002 	 2024-08-23T04:21:42.200Z
+INSERT INTO Users (id, firstName, lastName)
+Values ("100","Alan","Appleton");
+
+INSERT INTO Users (id, firstName, lastName)
+Values ("101","Bailey","Baker");
+
+INSERT INTO Users (id, firstName, lastName)
+Values ("102","Charlie","Clune");
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,11 +8,13 @@ const app = new Hono<{ Bindings: ENVS }>()
 app.get('/', async (c) => {
   const db = getDB(c.env.DB);
 
-  const result = await db.insertInto('Users').values({
-    id:"2",
-    firstName:"tyler",
-    lastName:"Beaumont",
-  }).executeTakeFirst()
+  // const result = await db.insertInto('Users').values({
+  //   id:"2",
+  //   firstName:"tyler",
+  //   lastName:"Beaumont",
+  // }).executeTakeFirst()
+
+  const result = await db.selectFrom('Users').selectAll().execute()
 
   return c.text(`Hello Hono!, ${JSON.stringify(result,(_, v) =>
     typeof v === "bigint" ? v.toString() : v,)}`)
